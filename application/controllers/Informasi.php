06268773a804805ee19e7bbeda0112990de2b2cc
Lovanto R <rifkylovanto@gmail.com>
2021-12-13 15:51:29 +0700

    show page infrmasi

--- application/controllers/Informasi.php
+++ application/controllers/Informasi.php
@@ -1,11 +1,9 @@
 <?php
-
-class Dashboard extends CI_Controller{
+class Informasi extends CI_Controller{
 	public function index(){
 		$this->load->view('templates/header');
 		$this->load->view('informasi/index');
 		$this->load->view('templates/footer');
 	}
 }
-
 ?>
--- application/views/informasi/index.php
+++ application/views/informasi/index.php
@@ -1,1 +1,61 @@
-INFORMASI
+<div class="d-flex pt-5" style="background-color: #FFF !important;">
+    <div class="p-5 col-md-10 shadow ml-container" style="border-radius: 30px;">
+        <h1 class="ms-5 mb-2">Get Touch with Us</h1>
+        <div class="row">
+            <div class="col-md-2 rounded2 text-center index-1 pt-4 ms-5">
+                <div class="card-profile">
+                    <div class="card-photo"><img src="./assets/image/profil_1.jpeg" class="img-fluid"></div>
+                    <!-- Team Details-->
+                    <div class="card-info-profile">
+                        <h5>Maulana Akbar</h5>
+                        <p>1302190074</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2 rounded2 text-center index-1 pt-4 ms-5">
+                <div class="card-profile">
+                    <div class="card-photo"><img src="./assets/image/profil_2.jpeg" class="img-fluid"></div>
+                    <!-- Team Details-->
+                    <div class="card-info-profile">
+                        <h5>Rifky Lovanto</h5>
+                        <p>1302194068</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2 rounded2 text-center index-1 pt-4 ms-5">
+                <div class="card-profile">
+                    <div class="card-photo"><img src="./assets/image/profil_3.jpeg" class="img-fluid"></div>
+                    <!-- Team Details-->
+                    <div class="card-info-profile">
+                        <h5>Gilang Gumelar</h5>
+                        <p>1302194089</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2 rounded2 text-center index-1 pt-4 ms-5">
+                <div class="card-profile">
+                    <div class="card-photo"><img src="./assets/image/profil_4.jpeg" class="img-fluid"></div>
+                    <!-- Team Details-->
+                    <div class="card-info-profile">
+                        <h5>Dzakwan Daffa</h5>
+                        <p>1302194008</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2 rounded2 text-center index-1 pt-4 ms-5">
+                <div class="card-profile">
+                    <div class="card-photo"><img src="./assets/image/profil_5.jpeg" class="img-fluid"></div>
+                    <!-- Team Details-->
+                    <div class="card-info-profile">
+                        <h5>Rizki Naufal</h5>
+                        <p>1302194098</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
