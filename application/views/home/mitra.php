8a9173fef1649d483b91d50215f4c1c2cc7a57a6
Dzakwan Daffa Hidayatullah <dzakwandh9@gmail.com>
2021-12-27 20:35:33 +0700

    tambah halaman become our mitra

--- application/views/home/mitra.php
+++ application/views/home/mitra.php
@@ -46,7 +46,35 @@
 
     <main>
         <section>
+        <div class="mt-2 col-md-4 mx-auto">
+                <form class="row justify-content-center" method="post" action="<?= base_url() ?>mitra/tambahMitra">
+                    <div style="margin-top:100px;">
+                        <label for="namaMitra" class="form-label">Nama Mitra</label>
+                        <input type="text" class="form-control" id="namaMitra" name="namaMitra">
+                        <br>
 
+                        <label for="alamat" class="form-label">Alamat Mitra</label>
+                        <input type="text" class="form-control" id="alamat" name="alamatMitra">
+                        <br>
+
+                        <label for="noHpMitra" class="form-label">No Handphone/Telephon</label>
+                        <input type="text" class="form-control" id="noHpMitra" name="noHpMitra">
+                        <br>
+
+                        <label for="jenisUsaha" class="form-label">Bidang Usaha</label>
+                        <input type="text" class="form-control" id="jenisUsaha" name="jenisUsaha">
+                        <br>
+
+                        <label for="deskripsi" class="form-label">Deskripsi Usaha</label>
+                        <textarea type="text" class="form-control" id="deskripsi" rows="6" name="deskripsi"></textarea>
+                        <br>
+                    </div>
+                    <div class="float-end mb-3">
+                        <a class="btn btn-danger" id="back" href="<?= base_url() ?>mitra">Batalkan</a>
+                        <button type="submit" class="btn btn-success" id="save">Simpan</button>
+                    </div>
+                </form>
+            </div>
         </section>
     </main>
 
