6a31de8e48c69685bb13fbf1176da1dbc0de99c3
Dzakwan Daffa Hidayatullah <dzakwandh9@gmail.com>
2021-12-27 21:24:15 +0700

    modal detail konjungan

--- application/views/kunjungan/index.php
+++ application/views/kunjungan/index.php
@@ -30,9 +30,40 @@
 									<td><?php echo $data->jenisUsaha ?></td>
 									<td><?php echo $data->statusKunjungan ?></td>
 									<td>
-										<a href="#" class="btn btn-sm btn-primary w-100 mb-2" id="update">Detail</a>
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                        Detail
+                                    </button>
 									</td>
 								</tr>
+                                    <!-- Ini modal -->
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                      <div class="modal-dialog">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Detail Kunjungan</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                          </div>
+                                          <div class="modal-body">
+                                            <table>
+                                                <tr>
+                                                    <td><?php echo $data->idPengunjung ?></td>
+                                                    <td><?php echo $data->namaPengunjung ?></td>
+                                                    <td><?php echo $data->alamatPengunjung ?></td>
+                                                    <td><?php echo $data->noHpPengunjung ?></td>
+                                                    <td><?php echo $data->umurPengunjung ?></td>
+                                                    <td><?php echo $data->jenisKelaminPengunjung ?></td>
+                                                    <td><?php echo $data->statusKesehatan ?></td>
+                                                </tr>
+                                            </table>
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <!-- Ini tutup modal -->
 								<?php
                                     $i++;    
                                 }
